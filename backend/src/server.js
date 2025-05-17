7a1eb95e7c7b440418eb2b88b2adb3d63c0dccc7
night-dreamer <charon.deadpan@gmail.com>
2025-05-17 15:29:48 +0800

    production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -11,13 +12,25 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
 app.use(express.json({ limit: "5mb" }));
 app.use(rateLimiter);
 
 // routes
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 await connectDB().then(() => {
   app.listen(PORT, () => {
